923c68c6b6353fbe0281369ea20366ff3a737438
Abi Vickery <vickeryabi@gmail.com>
2020-01-07 15:30:18 -0600

    basic JS

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var chaater = {
+    // health: 'undefined',
+    name: 'rock',
+    type: ['Pebble', 'rock', 'Ruby', 'Diamond', 'topaz', 'Petrified gum']
+}
+
+var character = {
+    name: 'Percy Mercy',
+    nic: 'Pim',
+    otherInfo: [
+        age = null,
+        gender = null
+    ]
+}
