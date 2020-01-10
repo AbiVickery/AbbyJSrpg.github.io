acfa6b59f6d8e206420b157750366b67f9a8005c
Abi Vickery <vickeryabi@gmail.com>
2020-01-10 13:56:46 -0600

    default settings for function scenarios

--- js/app.js
+++ js/app.js
@@ -16,8 +16,7 @@ alert('Page still in development!');
 } */
 
 function scenario1() {
-    alert('You fly around a home and a human is in the proccess of yawning. Do you-');
-    ans1 = prompt('A. Fly into their mouth, knowing you have a 50/50 chance of survival? or B. Land on their nose while their eyes are closed?');
+    alert('.');
     // takes answers
     if (ans1 == [
         'a',
@@ -25,7 +24,7 @@ function scenario1() {
         '1'
     ]) {
         //display story for answer 1 of scenario1
-        message('You zoom into the person\'s mouth, buzzing around in the dark cave. Its all wet and smells bad, ideal for you to live in if you wanted to. As you\'re floating around in the mouth, the person finishes yawning and closes their mouth. Only a few seconds after closing their mouth do they realize what has happened and immedeatly open it back up and start coughing and wheezing. You take this chance to escape! That was fun!');
+        message('.');
         console.log('answer 1/2');
     } else if (ans1 == [
         'b',
@@ -33,7 +32,7 @@ function scenario1() {
         '2'
     ]) {
         // display story for answer 2 of scenario1
-        message('You decide to land on their nose while their eyes are closed. What you didn\'t know was that they had a fly swatter in their hands, but that didn\'t stop you. They took the fly swatter and tried to get you, but you are to fast and quick on your feet, so instead of getting you, they end up hitting themselves in the face, leaving a red mark especially around the nose.');
+        message('.');
         console.log('answer 2/2');
     }
 }
