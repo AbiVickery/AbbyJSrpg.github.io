ae15f1215d59192394ea5bf74c2c69a340b5ba84
Abi Vickery <vickeryabi@gmail.com>
2020-01-13 16:08:44 -0600

    scenarios

--- js/app.js
+++ js/app.js
@@ -16,8 +16,8 @@ alert('Page still in development!');
 } */
 
 function scenario1() {
-    alert('.');
-    ans1 = prompt('.');
+    alert('Percy flies around a home and a human is in the proccess of yawning. Does Percy-');
+    ans1 = prompt('A. Fly into the human\'s mouth, knowing he has a 50/50 chance of survival? or B. Land on the human\'s nose while their eyes are closed?');
     // takes answers
     if (ans1 == [
         'a',
@@ -25,7 +25,7 @@ function scenario1() {
         '1'
     ]) {
         //display story for answer 1 of scenario1
-        alert('.');
+        alert('Percy zooms into the person\'s mouth, buzzing around in the dark cave. Its all wet and smells bad, ideal for him to live in if he wanted to. As Percy floats around in the mouth, the person finishes yawning and closes their mouth. Only a few seconds after closing their mouth do they realize what has happened and immedeatly open it back up and start coughing and wheezing. Percy takes this chance to escape! That was fun!');
         console.log('answer 1/2');
     } else if (ans1 == [
         'b',
@@ -33,14 +33,14 @@ function scenario1() {
         '2'
     ]) {
         // display story for answer 2 of scenario1
-        alert('.');
+        alert('Percy decides to land on their nose while their eyes are closed. What he didn\'t know was that the human had a fly swatter in their hands, but that didn\'t stop him. They took the fly swatter and tried to get Percy, but he is to fast and quick on his feet, so instead of getting him, they end up hitting themselves in the face, leaving a red mark especially around the nose.');
         console.log('answer 2/2');
     }
 }
 // scenario1();
 function scenario2() {
-    alert('.');
-    ans2 = prompt('.');
+    alert('Percy continues flying around, laughing about his previous adventure with that human, the look on their face after what he had done. He wasn\'t really paying attention to where he was going and got sucked into a vaccum cleaner. Percy is now stuck inside the vaccum, trying to think of what to do next. He has 2 choices.');
+    ans2 = prompt('He can- A. Wait until the vaccum gets emptied or turned off and then escape. or B. Fly into the mechanics and break the vaccum down.');
     // takes answers
     if (ans2 == [
         'a',
@@ -48,7 +48,7 @@ function scenario2() {
         '1'
     ]) {
         // display story for answer 1 of scenario 2
-        alert('.');
+        alert('He waits for the vaccum to finish and eventually turn off. Once it does turn off, he makes for the exit and flies away from the machine. Thank goodness he didn\'t try anything drastic, who knows how that would have ended...');
         console.log('answer 1/2');
     } else if (ans2 == [
         'b',
@@ -56,15 +56,15 @@ function scenario2() {
         '2'
     ]){
         // display story for answer 2 of scenario 2
-        alert('.');
+        alert('Percy goes to the inner mechanics of the vaccum and clogs it up with debris that was stuck to his legs. The whole vaccum starts making bad noises and it stops working. He takes this chance to excape the vaccum cleaner and fly away.');
         console.log('answer 2/2');
     }
 }
 // scenario2();
 
 function scenario3() {
-    alert('.');
-    ans3 = prompt('.');
+    alert('Percy decides to leave the house and go out to explore the town. This town is not as big as most but it is quite decent. He passes by a gas station, a library and a convenience store. Percy thinks about exploring one of those places but which one?');
+    ans3 = prompt('Does he- A. Explore the Gas Station? B. Explore the Library? or C. Explore the Convenience Store?');
     // takes answers
     if (ans3 == [
         'a',
@@ -72,7 +72,7 @@ function scenario3() {
         '1'
     ]) {
         // display story for answer 1 of scenario 3
-        alert('.');
+        alert('Percy decided to explore the gas station. There were only 2 gas pumps so it wasn\'t a very big station. They looked old as well as a bit unused, as if this gas station didn\'t get very many visitors. As for the inside, it was hardly stocked and he couldn\'t see anyone else. He honestly started to wonder if this place was even open... Eventually he was bored and decided to leave and explore the rest of town.');
         console.log('answer 1/3');
     } else if (ans3 == [
         'b',
@@ -80,7 +80,7 @@ function scenario3() {
         '2'
     ]) {
         // display story for answer 2 of scenario 3
-        alert('.');
+        alert('Percy decided to explore the library. The inside was not to bad. It was decorated to look like a nice library, except that it was really old. So old that some of the paint was starting to peel and chip. There were shelves with books covered in dust like they haven\'t seen the sun in forever. Speaking of sun, there was a broken window on the ceiling that let in sun light, making the whole place seem a bit magical. But there wern\'t any people in the library... Eventually he got bored after flying around the empty library and decided to go back out and explore the rest of the town.');
         console.log('answer 2/3');
     } else if (ans3 == [
         'c',
@@ -88,7 +88,7 @@ function scenario3() {
         '3'
     ]) {
         // display story for answer 3 of scenario 3
-        alert('.');
+        alert('Percy decided to explore the convenience store. The outside of the store was boring and placid, but the inside was quite different. There were shelves with all different kinds of items on them. People were busy walking around looking for objects through the store. There was one person that seemed a bit strange, so he went over to them to see what they were doing. When Percy was at eye level with them and they noticed him, they screamed as loud as they could. Percy guessed they must have an irational fear of insects and made his way out of the store quickly. Well, back to exploring the rest of the town.');
         console.log('answer 3/3');
     }
 }
@@ -142,8 +142,8 @@ function scenario5() {
 // scenario5();
 
 function scenario6() {
-    alert('.');
-    ans6 = prompt('.');
+    alert('Percy is now in a building that has lots of people in dress clothes. There\'s a stage with someone on it that looks nervous, like they are about to give a big speech. They have a microphone sitting in a microphone stand. Percy suddenly has an idea.');
+    ans6 = prompt('Does he- A. Go through with the idea? or B. Ignore the idea knowing he could be killed and continue exploring the building?');
     // takes answers
     if (ans6 == [
         'a',
@@ -151,7 +151,7 @@ function scenario6() {
         '1'
     ]) {
         // display story for answer 1 of scenario 6
-        alert('.');
+        alert('Percy decides to go through with his idea. First he sneaks over to the mic, making sure its plugged in (which is is of course). Then he lands on the microphone and makes the most annoying buzzing noises ever as loud as he can. The whole room is in an uproar as they think the noise is caused by someone in the back messing with the sound systems. Percy just sits there luaghing while buzzing until someone sees him sitting on the microphone and then- SPLAT! He has been smushed! But on the plus side, the person that smushed him didn\'t know that Percy was radioactive so now they have radioation poisoning. Poor them. POOR PERCY! Oh well, that\'s life. THE END!');
         console.log('answer 1/2');
     } else if (ans6 == [
         'b',
@@ -159,7 +159,7 @@ function scenario6() {
         '2'
     ]) {
         // display story for answer 2 of scenario 6
-        alert('.');
+        alert('Percy continues on with his adventure, exploring the building.');
         console.log('answer 2/2');
     }
 }
@@ -189,8 +189,8 @@ function scenario7() {
 }
 // scenario7();
 function scenario8() {
-    alert('.');
-    ans8 = prompt('.');
+    alert('Percy is currently flying around the town and he sees a lab-like structure on a hill by itself. Curiousity sets in and he contemplates whether or not he should explore it or just keep flying around the town.');
+    ans8 = prompt('Does he- A. Go and explore the Lab? or B. Keep exploring the town?');
     // takes answers
     if (ans8 == [
         'a',
@@ -198,7 +198,7 @@ function scenario8() {
         '1'
     ]) {
         // display story for answer 1 of scenario 8
-        alert('.');
+        alert('Percy decided to go and explore the lab, curious as to what might lie inside the halls of the sterile building.');
         console.log('answer 1/2');
     } else if (ans8 == [
         'b',
@@ -206,14 +206,14 @@ function scenario8() {
         '2'
     ]) {
         // display story for answer 2 of scenario 8
-        alert('.');
+        alert('Percy starts to lose interest in the building on the hill and continues exploring the town, but later on he keeps thinking about the building on the hill, what secrets it might hold. Eventually, he decides to go back and take a look around the lab, curiosity outrulling reason.');
         console.log('answer 2/2');
     }
 }
 // scenario8();
 function scenario9() {
-    alert('.');
-    ans9 = prompt('.');
+    alert('Percy is flying around the laboratory and he spots a strange device on the table. Normally when he sees devices like this, he ignores them but this one is tiny, small enough for him to operate. He looks at what different controls it has and there are 2 buttons. One is blue with a circle shape. The other is yellow with a square shape. On the back of the device is a warning not to use or there will be severe consequences. Being the tiny jerk he is, Percy ignores the warning and decides to use the device just to be funny. Which button does he press?');
+    ans9 = prompt('Does he press- A. The blue circle button? or B. The yellow square button?');
     // takes answers
     if (ans9 == [
         'a',
@@ -221,7 +221,7 @@ function scenario9() {
         '1'
     ]) {
         // display story for answer 1 of scenario 9
-        alert('.');
+        alert('At first, nothing happens. But then he hears a strange noise and suddenly there is a massive black hole forming in the room out of thin air. It swallows the entire earth and everything and everyone dies. THE END!');
         console.log('answer 1/2');
     } else if (ans9 == [
         'b',
@@ -229,14 +229,14 @@ function scenario9() {
         '2'
     ]) {
         // display story for answer 2 of scenario 9
-        alert('.');
+        alert('PErcy pushes the yellow button and suddenly it starts raining inside the lab. Apparently he didn\'t notice he was sitting under a shower head that has been attached to the ceiling. The liquid is a sort of yellow color. Its lemonade! YUM! While he sits there enjoying the lemonade, he wonders who made this device and why. Percy is happy with the lemonade but now he goes off to explore the rest of the lab.');
         console.log('answer 2/2');
     }
 }
 // scenario9();
 function scenario10() {
-    alert('.');
-    ans10 = prompt('.');
+    alert('Percy is now in a different part of the lab, exploring and kind of wondering where all the lab people are cause he hasnt seen any lately. He stumbles into a large warehouse-like room that has a huge rocket ship. His curiousity compels him! He goes inside the rocket and sees a control panel with all sorts of buttons and switches.');
+    ans10 = prompt('Does he- A. Press all the buttons and switches until something happens? or B. Only press one random button?');
     // takes answers
     if (ans10 = [
         'a',
@@ -244,7 +244,7 @@ function scenario10() {
         '1'
     ]) {
         // display story for answer 1 of scenario 10
-        alert('.');
+        alert('Percy presses all the buttons and switches to see what happens and for fun because why not? Lots of lights start flashing and noises go off, including what sounds like the engines starting up. Suddenly the rocket blasts right through the roof of the lab and starts heading up into the sky, breaking through the atmosphere. Percy looks out the window and sees that he is headed towards the moon! What will he do now? Will he be the first radioactive fly on the moon!?');
         console.log('answer 1/2');
     } else if (ans10 == [
         'b',
@@ -252,7 +252,7 @@ function scenario10() {
         '2'
     ]) {
         // display story for answer 2 of scenario 10
-        alert('.');
+        alert('Percy decided to only press one button and choose a random one. Suddenly a countdown starts and he hears the sound of the engines of the rocket turning on. He knows what that means... THE ROCKET IS GETTING READY TO LAUNCH!!! Percy and the rocket soon start making their way up, pushing through the earth\'s atmosphere, heading for the moon! What will happen now?');
         console.log('answer 2/2');
     }
 }
