2412dfabe9802dec91821cbe37dd37ca2e0bac0b
Abi Vickery <vickeryabi@gmail.com>
2020-01-22 15:22:07 -0600

    more code ideas

--- js/extra.js
+++ js/extra.js
@@ -24,10 +24,30 @@ function load() {
 
 // load();
 
-function myFunction() {
-    var person = prompt("Please enter your name", "insert_name_here");
-    if (person != null) {
-      document.getElementById("demo").innerHTML =
-      "Hello " + person + "! How are you today?";
+// function myFunction() {
+//     var person = prompt("Please enter your name", "insert_name_here");
+//     if (person != null) {
+//       document.getElementById("demo").innerHTML =
+//       "Hello " + person + "! How are you today?";
+//     }
+//   }
+
+
+  function myFunction() {
+    var text;
+    var favDrink = prompt("What's your favorite cocktail drink?", "Daiquiri");
+    switch(favDrink) {
+      case "Martini":
+        text = "Excellent choice. Martini is good for your soul.";
+        break;
+      case "Daiquiri":
+        text = "Daiquiri is my favorite too!";
+        break;
+      case "Cosmopolitan":
+        text = "Really? Are you sure the Cosmopolitan is your favorite?";
+        break;
+      default:
+        text = "I have never heard of that one..";
     }
+    document.getElementById("demo").innerHTML = text;
   }
